cb28e795a8e8df70c7b33e648fea3a596da05979
SusanLiu0405 <15301609750@163.com>
2023-05-01 06:04:42 -0400

    only final lest

--- project2/public/scripts/cats_big_cards.js
+++
@@ -1,9 +0,0 @@
-"use script";
-(function () {
-    const hamburgerEl = document.querySelector('.toggle-active');
-    const dropdownContentEl = document.querySelector('.dropdownContent');
-    hamburgerEl.addEventListener('click', function () {
-        dropdownContentEl.classList.toggle('active');
-    });
-
-})();
--- work/10-react-pages/pages/src/TextContent.jsx
+++
@@ -1,9 +0,0 @@
-import Paragraph from './Paragraph.jsx';
-import './TextContent.css';
-function TextContent() {
-    return (
-        <Paragraph className="paragraph" />
-    );
-}
-
-export default TextContent;
