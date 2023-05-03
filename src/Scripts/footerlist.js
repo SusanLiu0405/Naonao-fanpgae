5285b308bea4a76fd6fbac4fd4bf591fe39a7213
SusanLiu0405 <15301609750@163.com>
2023-05-03 04:20:16 -0400

    commit by May 3 2nd

---
+++ src/Scripts/footerlist.js
@@ -0,0 +1,31 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faWeibo } from '@fortawesome/free-brands-svg-icons';
+import { faBilibili } from '@fortawesome/free-brands-svg-icons';
+
+
+const footerlist = [
+    {
+        name: "Instagram",
+        path: "https://www.instagram.com",
+        icon: <FontAwesomeIcon icon={faInstagram} style={{color: "#fffffb",}} />,
+    },
+    {
+        name: "Facebook",
+        path: "https://www.facebook.com/susanliu01000101/",
+        icon: <FontAwesomeIcon icon={faFacebookF} style={{color: "#fffffb",}} />,
+    },
+    
+    {
+        name: "Weibo",
+        path: "https://weibo.com/AffectionateSilence",
+        icon: <FontAwesomeIcon icon={faWeibo} style={{color: "#fffffb",}} />,
+    },
+    {
+        name: "Bilibili",
+        path: "https://space.bilibili.com/5819125?spm_id_from=333.1007.0.0",
+        icon: <FontAwesomeIcon icon={faBilibili} style={{color: "#fffffb",}} />,
+    }
+]
+export default footerlist;
